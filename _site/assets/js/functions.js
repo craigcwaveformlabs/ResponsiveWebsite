8455414c49a1cdfc1a3acf8363b26c3da54930bb
craigcwaveformlabs <craigmclarke@gmail.com>
2015-10-26 15:39:22 +0000

    adding work section. adding scroll to view work details. adding functions to add new project

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,6 +1,8 @@
 $( document ).ready(function() {
 
   smoothScroll(1000);
+  workBelt();
+  workLoad();
 
 });
 
@@ -23,4 +25,37 @@ function smoothScroll (duration) {
 
 
   });
-}
+} // end of smoothScroll
+
+
+function workBelt() {
+
+  $('.thumb-unit').on('click', function() {
+    $('.work-belt').css('left','-100%');
+    $('.work-container').show();
+  });
+
+  $('.work-return').click(function() {
+    $('.work-belt').css('left','0%');
+    $('.work-container').hide(400);
+  });
+} // end of workBelt
+
+
+function workLoad() {
+
+  // set cache to TRUE when ready to release
+  $.ajaxSetup({ cache: false });
+
+  $('.thumb-unit').click(function() {
+
+    var $this = $(this),
+        newTitle = $this.find('strong').text(),
+        newFolder = $this.data('folder'),
+        spinner = '<div class="loader">Loading...</div>',
+        newHTML = 'work/' + newFolder + '.html';
+    $('.project-load').html(spinner).load(newHTML);
+    $('.project-title').text(newTitle);
+
+  });
+} // end of workLoad
